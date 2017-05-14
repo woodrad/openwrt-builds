9366bf8f39cd0a3f5c37938c9fb056aaec79e4e1
woodrad <mat@matwoodyard.com>
2017-05-14 10:51:12 -0500

    Build the machine using a StackScript.

--- make_user.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-adduser builder
-su - builder
--- stackscript.sh
+++ stackscript.sh
@@ -5,3 +5,8 @@ yum install -y htop tmux
 gpg --keyserver keys.fedoraproject.org --recv-key 352C64E5
 rpm -i http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
 yum install -y htop wget zlib-static openssl-devel ncurses-devel
+
+adduser builder
+su - builder
+
+https://github.com/woodrad/openwrt-builds.git
