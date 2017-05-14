5a71b5c4c3c4766d33c712fd10240b7dec80cded
woodrad <mat@matwoodyard.com>
2017-05-14 11:24:11 -0500

    Fix shebang and clone

--- stackscript.sh
+++ stackscript.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 yum update -y
 yum groupinstall -y "Development Tools"
 gpg --keyserver keys.fedoraproject.org --recv-key 352C64E5
@@ -6,6 +6,8 @@ rpm -i http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.r
 yum install -y htop tmux wget zlib-static openssl-devel ncurses-devel
 
 adduser builder
-su - builder
 
-git clone https://github.com/woodrad/openwrt-builds.git
+cd /home/builder
+su builder -c "git clone https://github.com/woodrad/openwrt-builds.git"
+cd /home/builder/openwrt-builds
+su builder -c "./get_openwrt.sh"
