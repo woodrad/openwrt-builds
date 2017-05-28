c2ee22e5f7077781dc42730c0488bec32f19ad63
woodrad <mat@matwoodyard.com>
2017-05-27 23:06:45 -0500

    Install epel properly.

--- stackscript.sh
+++ stackscript.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 yum update -y
 yum groupinstall -y "Development Tools"
-gpg --keyserver keys.fedoraproject.org --recv-key 352C64E5
-rpm -i http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
-yum install -y htop tmux wget zlib-static openssl-devel ncurses-devel
+yum install -y epel-release htop tmux wget zlib-static openssl-devel ncurses-devel
 
 adduser builder
 
